ea0151ccfd8a2dba86b6704eb92bd2315650b92a
xenia513 <xenia513@yandex.ru>
2026-03-11 01:44:01 +0400

    fix auth

--- playwright.config.js
+++ playwright.config.js
@@ -24,6 +24,7 @@ export default defineConfig({
     baseURL: 'http://localhost:5173',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    storageState: authFile,
   },
 
   /* Configure projects for major browsers */
@@ -37,7 +38,6 @@ export default defineConfig({
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
-        storageState: authFile,
       },
       dependencies: ['setup'],
     },
--- tests/auth.setup.js
+++ tests/auth.setup.js
@@ -3,15 +3,19 @@ import { test as setup, expect } from '@playwright/test'
 const authFile = 'user.json'
 
 setup('authenticate', async ({ page }) => {
-  await page.goto('http://localhost:5173/#/login', { waitUntil: 'networkidle' })
-
+  await page.goto('http://localhost:5173/#/login')
+  if (await page.locator('text=Dashboard').isVisible()) {
+    await page.context().storageState({ path: authFile })
+    return;
+  }
   await page.getByLabel('Username').fill('username')
   await page.getByLabel('Password').fill('password')
   await page.getByRole('button', { name: /Sign in/i }).click()
 
-  await expect(page).toHaveURL('http://localhost:5173/#/', { timeout: 50000 })
-  await expect(page.locator('h6')).toHaveText('Welcome to the administration', { timeout: 15000 })
+  await expect(page).toHaveURL('http://localhost:5173/#/', { timeout: 20000 })
+  await page.waitForFunction(() => localStorage.length > 0)
   await page.waitForLoadState('networkidle')
-  await page.waitForTimeout(1000)
+  await page.waitForTimeout(2000)
+  await expect(page.locator('h6')).toHaveText('Welcome to the administration', { timeout: 15000 })
   await page.context().storageState({ path: authFile })
 })
