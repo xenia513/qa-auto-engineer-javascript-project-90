94ece07b0ed4f931797aa8e87811b9581dc22a9c
xenia513 <xenia513@yandex.ru>
2026-03-11 14:37:40 +0400

    try to pass hexlet-check

--- tests/e2e/utils/BaseMVCPage.js
+++ tests/e2e/utils/BaseMVCPage.js
@@ -34,7 +34,7 @@ export default class BaseMVCPage {
       await this.page.locator('input[name="username"]').fill('username')
       await this.page.locator('input[name="password"]').fill('password')
       await this.page.getByRole('button', { name: /Sign in/i }).click()
-      await expect(this.page.locator('h6')).toHaveText('Welcome to the administration', { timeout: 15000 })
+      await expect(this.page).toHaveURL(new RegExp(expectedUrl), { timeout: 15000 })
 
       if (!this.page.url().includes(expectedUrl)) {
         await this.page.goto(expectedUrl)
